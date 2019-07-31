ee2faeea721d8660b3d5f77e94de638dc2d50acc
abdmun8 <abdmun8@gmail.com>
2019-07-31 11:40:12 +0700

    Update config

--- application/views/cms/dashboard.php
+++ application/views/cms/dashboard.php
@@ -1,62 +1,19 @@
 <section class="content-header">
   <h1>
-   Dashboard
+    Dashboard
   </h1>
   <ol class="breadcrumb">
-    <li><a href="<?php echo base_url();?>"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="<?php echo base_url(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
     <li class="active">Dashboard</li>
   </ol>
-   <!-- Right side column. Contains the navbar and content of the page -->
-        </section>
+  <!-- Right side column. Contains the navbar and content of the page -->
+</section>
 
-        <!-- Main content -->
-        <section class="content">
-          <!-- Info boxes -->
-          <div class="row">
-            <div class="col-md-3 col-sm-6 col-xs-12">
-              <div class="info-box">
-                <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
-                <div class="info-box-content">
-                  <span class="info-box-text">Barang Masuk</span>
-                  <span class="info-box-number">90</span>
-                </div><!-- /.info-box-content -->
-              </div><!-- /.info-box -->
-            </div><!-- /.col -->
-            <div class="col-md-3 col-sm-6 col-xs-12">
-              <div class="info-box">
-                <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
-                <div class="info-box-content">
-                  <span class="info-box-text">Barang Keluar</span>
-                  <span class="info-box-number">41</span>
-                </div><!-- /.info-box-content -->
-              </div><!-- /.info-box -->
-            </div><!-- /.col -->
+<!-- Main content -->
+<section class="content">
 
-            <!-- fix for small devices only -->
-            <div class="clearfix visible-sm-block"></div>
 
-            <div class="col-md-3 col-sm-6 col-xs-12">
-              <div class="info-box">
-                <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
-                <div class="info-box-content">
-                  <span class="info-box-text">Total Stock</span>
-                  <span class="info-box-number">49</span>
-                </div><!-- /.info-box-content -->
-              </div><!-- /.info-box -->
-            </div><!-- /.col -->
-            <div class="col-md-3 col-sm-6 col-xs-12">
-              <div class="info-box">
-                <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
-                <div class="info-box-content">
-                  <span class="info-box-text">Suplier</span>
-                  <span class="info-box-number">6</span>
-                      </ul>
-                    </div>
-                      </ul>
-                    </div>
-                   
-          
-        </section><!-- /.content -->
-      </div><!-- /.content-wrapper -->
-  </div>
+</section><!-- /.content -->
+</div><!-- /.content-wrapper -->
+</div>
 </section>
